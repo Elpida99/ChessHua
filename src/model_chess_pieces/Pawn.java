ed235a9f5140bae893897e2765cff852311c23cd
Elpida99 <61232574+Elpida99@users.noreply.github.com>
2020-04-11 15:31:32 +0300

    Update Pawn.java

--- src/model_chess_pieces/Pawn.java
+++ src/model_chess_pieces/Pawn.java
@@ -29,56 +29,68 @@ public class Pawn extends ChessPiece {
 	}
 
 	public boolean isMovePossible(ChessMove move, Board board) {
-		boolean answer = false; //answer: is the move possiple or not
-		move.setOldXY(getPiecePosition()); // piece's current position-->set it to class ChessMove
+		boolean answer = false; // answer: is the move possiple or not
+		move.setOldXY(this.getPiecePosition()); // piece's current position-->set it to class ChessMove
 		int row = move.getNewXY().getX(); // coordinates of desired move are the directions of the user
 		int col = move.getNewXY().getY();
-		System.out.println(row + " " + col); //check that they are right
-		if (this.isFirstMove()) { //the pawn can move 2 fields forward if it is its first move
-			// forward
-			if (board.isFieldOccupied(move.getOldXY().getX(), move.getOldXY().getY())) { //if piece exists on the field
-				System.out.println("piece exists on this field and it can be moved");
-				if (board.isFieldOccupied(row, col)) { //if new position is occupied, the pawn cannot move to that position
-					System.out.println("move not possible-field occupied");
-					answer = false;
-				} else {
-					System.out.println("position available");
-					if (col == getPiecePosition().getY() && Math.abs((row - getPiecePosition().getX())) <= 2) { //if column is the same-->the move is forward && not more than 2 fields
-						System.out.println("move is possible");
-						System.out.println(Math.abs((row - getPiecePosition().getX())));
-						answer = true;
+		System.out.println(row + " " + col); // check that they are right
+		if (ChessMove.isValid(col, row)) {
+			System.out.print("New position is valid, coordinates exist on board : ");
+			if (this.isFirstMove()) { // the pawn can move 2 fields forward if it is its first move
+				// forward
+				if (board.isFieldOccupied(move.getOldXY().getX(), move.getOldXY().getY())) { // if piece exists on the
+																								// field
+					System.out.print("piece exists on this field and it can be moved\n");
+					if (board.isFieldOccupied(row, col)) { // if new position is occupied, the pawn cannot move to that
+															// position
+						System.out.println("move not possible-new position occupied");
+						answer = false;
 					} else {
-						System.out.println("move not possible");
+						System.out.println("new position is available");
+						if (col == getPiecePosition().getY() && Math.abs((row - getPiecePosition().getX())) <= 2) { // if
+							// fields
+							System.out.println("move is possible-->move piece "
+									+ Math.abs((row - getPiecePosition().getX())) + " position(s) forward");
+							answer = true;
+						} else {
+							System.out.println("move not possible for this kind of piece");
+							answer = false;
+						}
+					}
+				} else {
+					System.out.println("This field " + move.getOldXY().getX() + " " + move.getOldXY().getY()
+							+ " does not contain a piece");
+				}
+			} else { // NOT FIRST MOVE
+				// forward
+				if (col == getPiecePosition().getY()) {
+					if (Math.abs((getPiecePosition().getX() - row)) == 1) {
+						answer = true;
+						System.out.println("move is possible-->move piece 1 position forward");
+					} else {
+						System.out.println("move not possible for this kind of piece");
 						answer = false;
 					}
-				}
-			}
-		} else { //NOT FIRST MOVE
-			// forward
-			if (col == getPiecePosition().getY()) {
-				if (Math.abs((getPiecePosition().getX() - row)) == 1) {
-					answer = true;
-					System.out.println("ELSE 2");
+					// diagonal
 				} else {
-					System.out.println("ELSE 2");
-					answer = false;
-				}
-			// diagonal	
-			}else {
-				if (Math.abs((getPiecePosition().getX() - row)) == 1 && Math.abs((getPiecePosition().getY() - col)) == 1) {
-					if(board.isFieldOccupied(row, col)) {
-						System.out.println("ELSE dia");
-						answer=true;
-					}else {
-						System.out.println("ELSE 2 dia");
-						answer=false;
+					if (Math.abs((getPiecePosition().getX() - row)) == 1
+							&& Math.abs((getPiecePosition().getY() - col)) == 1) {
+						if (board.isFieldOccupied(row, col)) {
+							System.out.println("Piece at field "+move.getNewXY().getX()+","+move.getNewXY().getY()+" is eaten by pawn");
+							answer = true;
+						} else {
+							System.out.println("new field empty : pawn can only move diagonally to eat another piece");
+							answer = false;
+						}
 					}
 				}
+
 			}
-			
+		} else {
+			System.out.println("new field must exist on board!");
 		}
-		
 		return answer;
 	}
+
 
 }
