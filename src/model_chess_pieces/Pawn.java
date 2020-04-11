61753b79829b268a45a9c97523755c3646b55077
Elpida99 <61232574+Elpida99@users.noreply.github.com>
2020-04-11 19:00:24 +0300

    Update Pawn.java

--- src/model_chess_pieces/Pawn.java
+++ src/model_chess_pieces/Pawn.java
@@ -29,16 +29,25 @@ public class Pawn extends ChessPiece {
 	}
 
 	public boolean isMovePossible(ChessMove move, Board board) {
-		boolean answer = false; // answer: is the move possiple or not
-		move.setOldXY(this.getPiecePosition()); // piece's current position-->set it to class ChessMove
-		int row = move.getNewXY().getX(); // coordinates of desired move are the directions of the user
-		int col = move.getNewXY().getY();
-		System.out.println(row + " " + col); // check that they are right
-		if (ChessMove.isValid(col, row)) {
+		boolean answer = false; // answer: is the move possible or not
+		String color = this.getColor().toString(); //colour of pawn
+		System.out.println(color);
+		move.setCurrent(this.getPiecePosition()); // piece's current position-->set it to class ChessMove
+		
+		int curRow = this.getPiecePosition().getRow(); //piece's current row
+		int curCol = this.getPiecePosition().getCol(); // piece's current column
+		
+		System.out.println("old coordinates: "+move.getCurrent().getRow()+","+move.getCurrent().getCol());
+				
+		int row = move.getNewPos().getRow(); // coordinates of desired move are the directions of the user
+		int col = move.getNewPos().getCol();
+		
+		System.out.println("new position: "+row+","+col); // check that they are right
+		if (ChessMove.isValid(row,col)) {
 			System.out.print("New position is valid, coordinates exist on board : ");
 			if (this.isFirstMove()) { // the pawn can move 2 fields forward if it is its first move
 				// forward
-				if (board.isFieldOccupied(move.getOldXY().getX(), move.getOldXY().getY())) { // if piece exists on the
+				if (board.isFieldOccupied(curRow,curCol)) { // if piece exists on the
 																								// field
 					System.out.print("piece exists on this field and it can be moved\n");
 					if (board.isFieldOccupied(row, col)) { // if new position is occupied, the pawn cannot move to that
@@ -47,10 +56,10 @@ public class Pawn extends ChessPiece {
 						answer = false;
 					} else {
 						System.out.println("new position is available");
-						if (col == getPiecePosition().getY() && Math.abs((row - getPiecePosition().getX())) <= 2) { // if
+						if (col== curCol && Math.abs((row - curRow)) <= 2) { // if
 							// fields
 							System.out.println("move is possible-->move piece "
-									+ Math.abs((row - getPiecePosition().getX())) + " position(s) forward");
+									+ Math.abs((row - curRow)) + " position(s) forward");
 							answer = true;
 						} else {
 							System.out.println("move not possible for this kind of piece");
@@ -58,13 +67,13 @@ public class Pawn extends ChessPiece {
 						}
 					}
 				} else {
-					System.out.println("This field " + move.getOldXY().getX() + " " + move.getOldXY().getY()
+					System.out.println("This field " + curRow + " " + curCol
 							+ " does not contain a piece");
 				}
 			} else { // NOT FIRST MOVE
 				// forward
-				if (col == getPiecePosition().getY()) {
-					if (Math.abs((getPiecePosition().getX() - row)) == 1) {
+				if (col == curCol) {
+					if (Math.abs((row-curRow)) == 1) {
 						answer = true;
 						System.out.println("move is possible-->move piece 1 position forward");
 					} else {
@@ -73,13 +82,13 @@ public class Pawn extends ChessPiece {
 					}
 					// diagonal
 				} else {
-					if (Math.abs((getPiecePosition().getX() - row)) == 1
-							&& Math.abs((getPiecePosition().getY() - col)) == 1) {
-						if (board.isFieldOccupied(row, col)) {
-							System.out.println("Piece at field "+move.getNewXY().getX()+","+move.getNewXY().getY()+" is eaten by pawn");
+					if (Math.abs((col-curCol)) == 1
+							&& Math.abs((row-curRow)) == 1) {
+						if (board.isFieldOccupied(row,col) && !(board.getField()[col][row].getChessPiece().getColor().toString().equals(color))) {
+							System.out.println("Piece at field "+row+","+col+" is eaten by pawn");
 							answer = true;
 						} else {
-							System.out.println("new field empty : pawn can only move diagonally to eat another piece");
+							System.out.println("new field empty : pawn can only move diagonally to eat another piece of different color");
 							answer = false;
 						}
 					}
@@ -91,6 +100,5 @@ public class Pawn extends ChessPiece {
 		}
 		return answer;
 	}
-
 
 }
