cca9adee18680841331dfc4839e3f3ace9939522
Eleftheria <it21754@hua.gr>
2020-04-11 04:26:51 +0300

    Update King.java

--- src/model_chess_pieces/King.java
+++ src/model_chess_pieces/King.java
@@ -5,33 +5,27 @@
  */
 package model_chess_pieces;
 
+import algorithm.ChessMove;
 import java.util.List;
+import model_board.Board;
 
 /**
  *
  * @author it21735 , it21754, it21762
  */
 public class King extends ChessPiece {
-    
+
     private final int value = 10;
-     
+
     private int[][] directions = {
-
-            {-1, -1},
-
-            {1, 1},
-
-            {1, -1},
-
-            {-1, 1},
-
-            {-1, 0},
-
-            {1, 0},
-
-            {0, -1},
-
-            {0, 1}
+        {-1, -1},
+        {1, 1},
+        {1, -1},
+        {-1, 1},
+        {-1, 0},
+        {1, 0},
+        {0, -1},
+        {0, 1}
     };
 
     public King(ChessPieceCharacteristics.Color color, ChessPieceCharacteristics.Name name) {
@@ -42,11 +36,44 @@ public class King extends ChessPiece {
     public int getValue() {
         return value;
     }
-    
- 
-    
-    
 
-    
-    
+    public boolean isMovePossible(ChessMove move, Board board) {
+        boolean answer = false; //answer: is the move possiple or not
+        move.setOldXY(getPiecePosition()); // piece's current position-->set it to class ChessMove
+        int row = move.getNewXY().getX(); // coordinates of desired move are the directions of the user
+        int col = move.getNewXY().getY();
+        System.out.println(row + " " + col); //check that they are right
+
+        if (board.isFieldOccupied(move.getOldXY().getX(), move.getOldXY().getY())) { //if piece exists on the field
+            System.out.println("piece exists on this field and it can be moved");
+            if (board.isFieldOccupied(row, col)) { //if new position is occupied, the pawn cannot move to that position
+                System.out.println("move not possible-field occupied");
+                answer = false;
+            } else {
+                System.out.println("position available-field not occupied    Let's check if the new move meets the \"criteria\"");
+                //row = new x 
+                if //VERTICAL 
+                        (col == getPiecePosition().getY() && (Math.abs((row - getPiecePosition().getX())) == 1 || Math.abs((row - getPiecePosition().getX())) == -1)) { //if column(Yposition) is the same-->the move is forward/ backword : newX - oldX = 1 or -1
+                    System.out.println("VERTICAL move is possible");
+                    System.out.println(Math.abs((row - getPiecePosition().getX())));
+                    answer = true;
+                }//HORIZONTAL 
+                else if (row == getPiecePosition().getX() && (Math.abs((col - getPiecePosition().getY())) == 1 || Math.abs((col - getPiecePosition().getY())) == -1)) {   //if row(Xposition)is the same and newY - oldY = 1 or -1
+                    System.out.println("HORIZONTAL move is possible");
+                    System.out.println(Math.abs((row - getPiecePosition().getX())));
+                    answer = true;
+                }//DIAGONAL 
+                else if ((Math.abs((row - getPiecePosition().getX())) == 1 || Math.abs((row - getPiecePosition().getX())) && (Math.abs((col - getPiecePosition().getY())) == 1 || Math.abs((col - getPiecePosition().getY())) {    //newX - oldX = 1 or -1 AND newY - oldY = 1 or -1
+                    System.out.println("DIAGONAL  move is possible");
+                    System.out.println(Math.abs((row - getPiecePosition().getX())));
+                    answer = true;
+                } else {
+                    System.out.println("move not possible");
+                    answer = false;
+                }
+            }
+        }
+        return answer;
+    }
+
 }
