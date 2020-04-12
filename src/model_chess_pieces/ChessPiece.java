4fcc07d1748d916f8e1b7c3e9f7bd510d9c46909
Elpida99 <61232574+Elpida99@users.noreply.github.com>
2020-04-12 20:26:21 +0300

    Update ChessPiece.java

--- src/model_chess_pieces/ChessPiece.java
+++ src/model_chess_pieces/ChessPiece.java
@@ -55,7 +55,22 @@ public abstract class ChessPiece {
         this.isAlive = isAlive;
     }
 	
+	public void makeMove(ChessMove move, Board board) {
 
+		int curRow = this.getPiecePosition().getRow(); // piece's current row
+		int curCol = this.getPiecePosition().getCol(); // piece's current column
+
+		int row = move.getNewPos().getRow(); // coordinates of desired move are the directions of the user
+		int col = move.getNewPos().getCol();
+
+		System.out.println("Current position: " + curRow + "," + curCol);
+		System.out.println("New position: " + row + "," + col);
+
+		board.getField()[curRow][curCol].removeChessPiece(); // remove piece from current position
+		board.getField()[row][col].setChessPiece(this); // set it to the new field
+
+	}
+	
 	public boolean isBlocked(Board board) {
 		boolean blocked = false;
 		boolean white = false;
