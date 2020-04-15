fe567c7c920907b9e0c371681b9e1aa0fd61b30a
Elpida99 <61232574+Elpida99@users.noreply.github.com>
2020-04-15 20:39:03 +0300

    Update ChessPiece.java

--- src/model_chess_pieces/ChessPiece.java
+++ src/model_chess_pieces/ChessPiece.java
@@ -27,7 +27,7 @@ public abstract class ChessPiece {
     
     //public abstract boolean isMovePossible();
 
-    public abstract List<Field> allPossibleMoves(Board board);
+    public abstract List<Field> allPossibleMoves(Board board,Player player); //player needed for pawn
 
     public abstract int getValue();
 
