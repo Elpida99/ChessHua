8741f9880efbec32323bde6152ee8ac0ee9f7b6f
Elpida99 <61232574+Elpida99@users.noreply.github.com>
2020-04-21 18:04:12 +0300

    Update miniMax.java

--- src/algorithm/miniMax.java
+++ src/algorithm/miniMax.java
@@ -106,32 +106,32 @@ public class miniMax {
         return allMoves;
     }
 
-   // gets all the moves of the color passed to it
-	public static List<ChessMove> getAllMoves(Board board, ChessPieceCharacteristics.Color colorType) {
-		// empty list that holds all the moves
-		List<ChessMove> playerMoves = new LinkedList<>();
-		for (int i = 0; i < 8; i++) {
-			for (int j = 0; j < 8; j++) {
-				// if that particular position has a piece that is the same color as the AI
-				if (board.isFieldOccupied(i, j) && board.getField()[i][j].getChessPiece().getColor() == colorType) {
-					try {
-						possibleMoves = board.getField()[i][j].getChessPiece().allPossibleMoves(board, player);
+  // gets all the moves of the color passed to it
+		public static List<ChessMove> getAllMoves(Board board, ChessPieceCharacteristics.Color colorType) {
+			// empty list that holds all the moves
+			List<ChessMove> playerMoves = new LinkedList<>();
+			for (int i = 0; i < 8; i++) {
+				for (int j = 0; j < 8; j++) {
+					// if that particular position has a piece that is the same color as the AI
+					if (board.isFieldOccupied(i, j) && board.getField()[i][j].getChessPiece().getColor() == colorType) {
+						try {
+							possibleMoves = board.getField()[i][j].getChessPiece().allPossibleMoves(board, player);
 
-						for (Field field : possibleMoves) {
-							playerMoves.add(
-									new ChessMove(field.getFieldCoordintes(), board.getField()[i][j].getChessPiece()));
+							for (Field field : possibleMoves) {
+								playerMoves.add(
+										new ChessMove(field.getFieldCoordintes(), board.getField()[i][j].getChessPiece()));
+							}
+
+						} catch (Exception e) {
+
 						}
-
-					} catch (Exception e) {
-
 					}
 				}
+
 			}
 
+			return playerMoves;
 		}
-
-		return playerMoves;
-	}
 
     private int Min(Board board, int alpha, int beta, int boardDepth) {
 
