23a1b99d7cb7ac577e4f737d01e22f1473ef09ae
Elpida99 <61232574+Elpida99@users.noreply.github.com>
2020-04-22 19:16:13 +0300

    Update Knight.java

--- src/model_chess_pieces/Knight.java
+++ src/model_chess_pieces/Knight.java
@@ -14,91 +14,101 @@ import model_board.FieldCoordinates;
  */
 public class Knight extends ChessPiece {
 
-	private final int value = 3;
+    private final int value = 3;
 
-	private int[][] directions = { { 1, 2 }, { -1, 2 }, { 2, -1 }, { -2, -1 }, { -1, -2 }, { 1, -2 }, { -2, 1 },
-			{ 2, 1 } }; // {vertical,horizontal}
+    private int[][] directions = {{1, 2}, {-1, 2}, {2, -1}, {-2, -1}, {-1, -2}, {1, -2}, {-2, 1},
+    {2, 1}}; // {vertical,horizontal}
 
-	public Knight(ChessPieceCharacteristics.Color color, ChessPieceCharacteristics.Name name) {
-		super(color, name);
-	}
+    public Knight(ChessPieceCharacteristics.Color color, ChessPieceCharacteristics.Name name) {
+        super(color, name);
+    }
 
-	@Override
-	public int getValue() {
-		return value;
-	}
+    @Override
+    public int getValue() {
+        return value;
+    }
 
-	public List<Field> allPossibleMoves(Board board,Player player) {
-		List<Field> moves = new LinkedList<>();
-		int currow = this.getPiecePosition().getRow();
-		int curcol = this.getPiecePosition().getCol();
-		ChessMove move = new ChessMove(null, this);
+    public List<Field> allPossibleMoves(Board board, Player player) {
+        List<Field> moves = new LinkedList<>();
+        int currow = this.getPiecePosition().getRow();
+        int curcol = this.getPiecePosition().getCol();
+        ChessMove move = new ChessMove(null, this);
 
-		moves.add(board.getField()[currow][curcol]);
+        //	moves.add(board.getField()[currow][curcol]);
+        for (int i = 0; i < this.directions.length; i++) {
+            int[] direction = directions[i];
+            int col = curcol + direction[0];
+            int row = currow + direction[1];
+            move.setNewCoor(row, col);
+            if (this.isMovePossible(move, board)) {
+                moves.add(new Field(row, col));
+            }
 
-		for (int i = 0; i < this.directions.length; i++) {
-			int[] direction = directions[i];
-			int col = curcol + direction[0];
-			int row = currow + direction[1];
-			move.setNewCoor(row, col);
-			if (this.isMovePossible(move, board)) {
-				moves.add(new Field(row, col));
-			}
+        }
+        return moves;
 
-		}
-		return moves;
+    }
 
-	}
+    public boolean isMovePossible(ChessMove move, Board board) {
+        boolean answer = false; // final answer--is the requested move possible?
+        boolean validPosition = false; // temp boolean to check if the move is valid without checking if field is
+        // occupied
 
-	public boolean isMovePossible(ChessMove move, Board board) {
-		boolean answer = false; // final answer--is the requested move possible?
-		boolean validPosition = false; // temp boolean to check if the move is valid without checking if field is
-										// occupied
+        String colour = this.getColor().toString(); // colour of piece
 
-		String colour = this.getColor().toString(); // colour of piece
-		System.out.println(colour + this.getName());
+        int currow = this.getPiecePosition().getRow(); // current row
+        int curcol = this.getPiecePosition().getCol(); // current column
 
-		int currow = this.getPiecePosition().getRow(); // current row
-		int curcol = this.getPiecePosition().getCol(); // current column
+        int row = move.getNewPos().getRow(); // requested new coordinates
+        int col = move.getNewPos().getCol();
 
-		int row = move.getNewPos().getRow(); // requested new coordinates
-		int col = move.getNewPos().getCol();
+        if (ChessMove.isValid(row, col)) { // do the new coordinates exist on board?
 
-		if (ChessMove.isValid(row, col)) { // do the new coordinates exist on board?
-			System.out.println("new coordinates exist on board---let's check if the move is valid");
+            // 2 vertical + 1 horizontal
+            if ((Math.abs(col - curcol) == 1) && Math.abs(row - currow) == 2) { // if column changes by 1 and row by
+                // 2 fields
+                validPosition = true; // possible move---> 2 fields vertically and 1 horizontally
 
-			// 2 vertical + 1 horizontal
-			if ((Math.abs(col - curcol) == 1) && Math.abs(row - currow) == 2) { // if column changes by 1 and row by
-																				// 2 fields
-				validPosition = true; // possible move---> 2 fields vertically and 1 horizontally
+                // 2 horizontal + 1 vertical
+            } else if ((Math.abs(col - curcol) == 2) && Math.abs(row - currow) == 1) { // if column changes by 2 and
+                // row by one field
+                validPosition = true; // possible move---> 2 fields horizontally and 1 vertically
 
-				// 2 horizontal + 1 vertical
-			} else if ((Math.abs(col - curcol) == 2) && Math.abs(row - currow) == 1) { // if column changes by 2 and
-																						// row by one field
-				validPosition = true; // possible move---> 2 fields horizontally and 1 vertically
+            } else { // no other move is possible for the knight
+            }
+            if (validPosition) { // if valid position is true, we must check if the field is occupied or not and
+                // by what colour
+                if ((board.isFieldOccupied(row, col))
+                        && !(board.getField()[row][col].getChessPiece().getColor().toString().equals(colour))) {
+                    // if occupied by different colour-it gets eaten
+                    move.setAttack(true);
+                    answer = true;
+                } else if (!board.isFieldOccupied(row, col)) { // if not occupied, the position is free for the
+                    // knight
+                    answer = true;
+                }
+            }
+        }
 
-			} else { // no other move is possible for the knight
-				System.out.println("Move not possible for this kind of piece");
-			}
-			if (validPosition) { // if valid position is true, we must check if the field is occupied or not and
-									// by what colour
-				if ((board.isFieldOccupied(row, col))
-						&& !(board.getField()[row][col].getChessPiece().getColor().toString().equals(colour))) {
-					// if occupied by different colour-it gets eaten
-					System.out.println("Piece at " + row + "," + col + " is eaten by the knight!");
-					answer = true;
-				} else if (!board.isFieldOccupied(row, col)) { // if not occupied, the position is free for the
-																// knight
-					System.out.println("Knight is moved from " + currow + "," + curcol + " to " + row + "," + col);
-					answer = true;
-				} else {
-					System.out.println("Position not available -- taken by another piece");
-				}
-			}
-		} else {
-			System.out.println("New position does not exist on the board!");
-		}
+        return answer;
+    }
 
-		return answer;
-	}
+    @Override
+    public void makeMove(ChessMove move, Board board) {
+
+        int curRow = this.getPiecePosition().getRow(); // piece's current row
+        int curCol = this.getPiecePosition().getCol(); // piece's current column
+
+        int row = move.getNewPos().getRow(); // coordinates of desired move are the directions of the user
+        int col = move.getNewPos().getCol();
+
+        if (move.getAttack()) {
+            board.getField()[row][col].getChessPiece().setIsAlive(false);
+            board.getField()[row][col].removeChessPiece();
+            // board.getField()[row][col].getChessPiece().setPiecePosition(null);
+        }
+        board.getField()[curRow][curCol].removeChessPiece(); // remove piece from current position
+        board.getField()[row][col].setChessPiece(this); // set it to the new field
+
+    }
 }
