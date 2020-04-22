699d82aa5ffa8e6fe43976eb0f676abbf1d85277
Eleftheria <it21754@hua.gr>
2020-04-22 18:02:35 +0300

    Update Rook.java

--- src/model_chess_pieces/Rook.java
+++ src/model_chess_pieces/Rook.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package model_chess_pieces;
 
 import algorithm.ChessMove;
@@ -85,48 +80,36 @@ public class Rook extends ChessPiece {
 
         answer = false; //answer: is the move possiple or not
         String color = this.getColor().toString(); // colour of pawn
-        System.out.println("colour=" + color);
+        //System.out.println("colour=" + color);
         move.setCurrent(this.getPiecePosition()); // piece's current position-->set it to class ChessMove
 
         curRow = this.getPiecePosition().getRow(); // piece's current row , x !!
         curCol = this.getPiecePosition().getCol(); // piece's current column, y !!
-        System.out.println("old coordinates: " + move.getCurrent().getRow() + "," + move.getCurrent().getCol());
+        //System.out.println("old coordinates: " + move.getCurrent().getRow() + "," + move.getCurrent().getCol());
 
         row = move.getNewPos().getRow(); // coordinates of desired move are the directions of the user
         col = move.getNewPos().getCol();
-        System.out.println("new position: " + row + "," + col); // check that they are right
+        //System.out.println("new position: " + row + "," + col); // check that they are right
 
-        if (board.isFieldOccupied(curRow, curCol)) { //if piece exists on the field
-            System.out.println("piece exists on this field and it can be moved\n");
-
-            if (ChessMove.isValid(row, col)) { //if the new position is valid (row&col <8)
-                System.out.print("New position is valid, coordinates exist on board (row&col <8) ");
+        if (board.isFieldOccupied(curRow, curCol) && ChessMove.isValid(row, col)) { //if piece exists on the field and the new position is valid (row&col <8)
+            //System.out.println("piece exists on this field and it can be moved and New position is valid, coordinates exist on board (row&col <8) \n");
 
                 if (board.isFieldOccupied(row, col) && !(board.getField()[row][col].getChessPiece().getColor().toString().equals(color))) {
                     //if NEW POSITION IS OCCUPIED/ NOT EMPTY , we need to check the colour of the pioni + and the colour is DIFFERENT from the one that the king has 
-                    System.out.println("field occupied-the colour of the pawn (pioni) needs to be checked ");
+                    //System.out.println("field occupied-the colour of the pawn (pioni) needs to be checked ");
 
                     check_col_row(col, curCol, row, curRow);
-
                 } else if (board.isFieldOccupied(row, col) && (board.getField()[row][col].getChessPiece().getColor().toString().equals(color))) {
                     //if NEW POSITION IS OCCUPIED/ NOT EMPTY , we need to check the colour of the pioni + and the colour is the SAME from the one that the king has 
-                    System.out.println("field occupied-the colour of the pawn (pioni) is the SAME with the colour of the king ");
+                    //System.out.println("field occupied-the colour of the pawn (pioni) is the SAME with the colour of the king ");
                     answer = false;
                 } else if (!(board.isFieldOccupied(row, col))) { //FIELD EMPTY
-                    System.out.println("position available-FIELD NOT OCCUPIED/EMPTY  --- Let's check if the new move meets the \"criteria\"");
-
+                   // System.out.println("position available-FIELD NOT OCCUPIED/EMPTY  --- Let's check if the new move meets the \"criteria\"");
                     check_col_row(col, curCol, row, curRow);
                 }
-
-            } else {
-                // not valid, out of bounds
-                System.out.println("new field must exist on board!");
-                answer = false;
-                //+while loop for checking a new entry 
-            }
         } else {
-            System.out.println("piece DOES NOT exists on this field -- move not possible");
-            System.out.println("This field " + curRow + " " + curCol + " does not contain a piece");
+           // System.out.println("piece DOES NOT exists on this field -- move not possible");
+            //System.out.println("This field " + curRow + " " + curCol + " does not contain a piece");
             answer = false;
         }
         return answer;
@@ -137,16 +120,16 @@ public class Rook extends ChessPiece {
         //curRow = old x, curCol = old y
         if //VERTICAL 
                 (col == curCol) { //if column(Yposition) is the same-->the move is forward/ backword : newX - oldX = 1 or -1
-            System.out.println("VERTICAL move is possible");
+            //System.out.println("VERTICAL move is possible");
             System.out.println(Math.abs((row - curRow)));
             answer = true;
         }//HORIZONTAL 
         else if (row == curRow) {   //if row(Xposition)is the same and newY - oldY = 1 or -1
-            System.out.println("HORIZONTAL move is possible");
+            //System.out.println("HORIZONTAL move is possible");
             System.out.println((row - curRow));
             answer = true;
         } else {
-            System.out.println("piece exists on this field -- move not possible");
+            //System.out.println("piece exists on this field -- move not possible");
             answer = false;
         }
     }
