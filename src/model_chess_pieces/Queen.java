d5147851bb90fb4eef3cd30b99187b2a8ef33888
Eleftheria <it21754@hua.gr>
2020-04-23 19:47:45 +0300

    Update Queen.java

--- src/model_chess_pieces/Queen.java
+++ src/model_chess_pieces/Queen.java
@@ -201,7 +201,7 @@ public class Queen extends ChessPiece {
                 int col = curCol + direction[0] * j;
                 int row = curRow + direction[1] * j;
 
-                move.setNewCoor(row, col);
+               // move.setNewCoor(row, col);
                 //if the possible position is valid check the next positions
                 if (this.isMovePossible(move, board)) {
 
