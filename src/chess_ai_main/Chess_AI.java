d4250c4bbf6507d319401efaae933dc263ee24d9
Eleftheria <it21754@hua.gr>
2020-04-26 03:13:49 +0300

    Update Chess_AI.java

--- src/chess_ai_main/Chess_AI.java
+++ src/chess_ai_main/Chess_AI.java
@@ -5,11 +5,7 @@
  */
 package chess_ai_main;
 
-import algorithm.*;
-import java.util.LinkedList;
-import java.util.List;
-import model_board.*;
-import model_chess_pieces.*;
+import algorithm.Game;
 
 /**
  *
@@ -17,62 +13,15 @@ import model_chess_pieces.*;
  */
 public class Chess_AI {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        
+       public static void main(String[] args) {
         // TODO code application logic here
         Game game = new Game();
-        game.ChessGame();
-        
-        /*
-        //tests for pawn
-        
-        ChessPiece piece = board.getField()[6][7].getChessPiece();
-		Pawn pawn = new Pawn(null, null);
-		pawn = (Pawn) piece;
-        FieldCoordinates newpos = new FieldCoordinates(3, 4);
-        ChessMove move = new ChessMove(newpos,pawn);
-        
-        //test for bishop
-       ChessMove bMove = new ChessMove();
-        Bishop bishop = new Bishop(ChessPieceCharacteristics.Color.b,ChessPieceCharacteristics.Name.B);
-        
-        
-        bishop.setPiecePosition(board.getField()[6][4].getFieldCoordintes());
-        System.out.println(board.getField()[6][4].getFieldCoordintes());
-        
-        bMove.setP(bishop);
-        FieldCoordinates newBPos = new FieldCoordinates(3,5);
-        bMove.setNewPos(newBPos);
-        System.out.println(bishop.isMovePossible(bMove, board)); 
-        
-        //test for rook 
-        ChessPiece piece1 = board.getField()[7][0].getChessPiece();
-        Rook rook = (Rook) piece1;
-        rook.setPiecePosition(piece1.getPiecePosition());
-        List<Field> moves_rook = new LinkedList<>();
-        moves_rook = rook.allPossibleMoves(board);
-        for (int i = 0; i < moves_rook.size(); i++) {
-            System.out.println("possible fields: " + moves_rook.get(i).getFieldCoordintes() + "\n");
-        }
-        
-        //test for king 
-        ChessPiece piece = board.getField()[7][4].getChessPiece();
-        King king = (King) piece;
-        king.setPiecePosition(piece.getPiecePosition());
-        List<Field> moves = new LinkedList<>();
-        moves = king.allPossibleMoves(board);
-        for (int i = 0; i < moves.size(); i++) {
-            System.out.println("possible fields: " + moves.get(i).getFieldCoordintes() + "\n");
-        }
-        
-        //test for miniMax
-        miniMax alg = new miniMax();
-		alg.AIColor=ChessPieceCharacteristics.Color.w;
-
-		System.out.println("Evaluation of board: "+alg.evaluateBoard(board));
-*/
+	game.ChessGame();
     }
 }
+
+
+
+
+
+                       
